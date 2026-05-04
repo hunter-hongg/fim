794c1189033d36fde0bed497f93355d4110a3de0
hunter-hongg <2843249813@qq.com>
2026-05-04 08:16:11 +0800

    feat: 添加clap依赖

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+/// A simple file inspection tool
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// File to open
+    file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    println!("Opening file: {}", cli.file);
 }
